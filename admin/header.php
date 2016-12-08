cfa9ed9265eba586b8d5f473e6c84211ba7a0080
sameer kumar <kumarsameer1992@gmail.com>
2016-12-08 09:58:26 -0500

    Final Project

---
+++ admin/header.php
@@ -0,0 +1,5 @@
+<!-- Sameer Kumar Kotra -->
+<li><a href="adminIndex.php">Home</a></li>
+<li><a href="users.php">Users</a></li>
+<li><a href="messages.php">Messages</a></li>
+<li><a href="../logout.php">Logout</a></li>
---
+++ user/header.php
@@ -0,0 +1,4 @@
+<!-- Sameer Kumar Kotra -->
+<li><a href="userIndex.php">Home</a></li> 
+<li><a href="messages.php">Messages</a></li>
+<li><a href="../logout.php">Logout</a></li>
